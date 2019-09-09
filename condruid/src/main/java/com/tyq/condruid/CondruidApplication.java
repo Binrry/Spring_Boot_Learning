6f1440a2746320fb5296feafd44df70af8672e08
Binrry <184471715@qq.com>
2019-09-09 20:32:56 +0800

    Unit Two ConDruid

---
+++ condruid/src/main/java/com/tyq/condruid/CondruidApplication.java
@@ -0,0 +1,13 @@
+package com.tyq.condruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CondruidApplication.class, args);
+    }
+
+}
